2cb148955500b675d40ee4525f532369c425c0e6
Shayan <zamani.shayan@yahoo.com>
2020-10-09 17:38:11 -0600

    fixed the bug about winston console options

--- startup/logging.js
+++ startup/logging.js
@@ -4,7 +4,12 @@ require("express-async-errors");
 
 module.exports = function () {
   winston.exceptions.handle(
-    new winston.transports.Console({ colorize: true, prettyPrint: true }),
+    new winston.transports.Console({
+      format: winston.format.combine(
+        winston.format.colorize(),
+        winston.format.simple()
+      ),
+    }),
     new winston.transports.File({ filename: "uncaughtExceptions.log" })
   );
   process.on("uncaughtException", (ex) => {
