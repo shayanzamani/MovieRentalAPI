d15088a906a3f85f86a38634207bef5ea3024537
Shayan <zamani.shayan@yahoo.com>
2020-10-09 10:23:17 -0600

    hash password using bcrypt salting package

--- routes/users.js
+++ routes/users.js
@@ -1,3 +1,4 @@
+const bcrypt = require("bcrypt");
 const _ = require("lodash");
 const { User, validate } = require("../models/user");
 const express = require("express");
@@ -16,6 +17,8 @@ router.post("/", async (req, res) => {
   if (user) return res.status(400).send("User already registered!");
 
   let user = new User(_.pick(req.body, ["name", "email", "password"]));
+  const salt = await bcrypt.genSalt(10);
+  user.password = await bcrypt.hashSync(user.password, salt);
   await user.save();
 
   res.send(_.pick(user, ["name", "email"]));
