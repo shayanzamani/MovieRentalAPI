8831bf5b2d8d30533a679d12fc3f657f323abeac
Shayan <zamani.shayan@yahoo.com>
2020-10-09 10:15:45 -0600

    use lodash to pick and return selected fields

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,5 @@
+const _ = require("lodash");
 const { User, validate } = require("../models/user");
-const mongoose = require("mongoose");
 const express = require("express");
 const router = express.Router();
 
@@ -15,14 +15,10 @@ router.post("/", async (req, res) => {
   let user = await User.findOne({ email: req.body.email });
   if (user) return res.status(400).send("User already registered!");
 
-  let user = new User({
-    name: req.body.name,
-    email: req.body.email,
-    password: req.body.password,
-  });
+  let user = new User(_.pick(req.body, ["name", "email", "password"]));
   await user.save();
 
-  res.send(user);
+  res.send(_.pick(user, ["name", "email"]));
 });
 
 module.exports = router;
